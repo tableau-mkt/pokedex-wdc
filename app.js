fbb4da93b72459470b22419b2931bc289bbc66d6
Etroid <mkeereman@tableau.com>
2016-07-19 12:46:26 -0700

    Root

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
     port = process.env.PORT || 3000,
     app = express();
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname));
 
 app.listen(port, function () {
   console.log("Pokedex Web Data Connector is running on port " + port);
